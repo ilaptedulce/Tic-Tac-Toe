d97d58cc586a357662dd59106279a51d19df9899
Irina Laptedulce <Irina.Laptedulce@endava.com>
2022-09-22 23:17:31 +0300

    added tic-tac-toe board html/css.js

---
+++ script.js
@@ -0,0 +1,41 @@
+let board
+const playerO = 'O'
+const playerX = 'X'
+let currPlayer = playerO
+const gameOver = false
+
+window.onload = function () { SetGame() }
+
+function SetGame () {
+  board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+  ]
+  for (let r = 0; r < 3; r++) {
+    for (let c = 0; c < 3; c++) {
+      const tile = document.createElement('div')
+      tile.id = r.toString() + '-' + c.toString()
+      tile.classList.add('tile')
+
+      if (r === 0 || r === 1) {
+        tile.classList.add('horizontal-line')
+      }
+      if (c === 0 || c === 1) {
+        tile.classList.add('vertical-line')
+      }
+      tile.addEventListener('click', setTile)
+      document.getElementById('board').append(tile)
+    }
+  }
+}
+
+function setTile () {
+  if (gameOver) {
+    return
+  }
+  let coords = this.id.split('-')
+  let r = parseInt(coords[0])
+  let c = parseInt(coords[1])
+  board[r][c] = currPlayer
+}

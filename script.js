5e6fa6358e972d7f5ac47649a0d8da8c89733d03
Irina Laptedulce <Irina.Laptedulce@endava.com>
2022-10-06 22:27:39 +0300

    added other version of game

--- script.js
+++ script.js
@@ -1,105 +1,119 @@
-let board
-const playerO = 'O'
-const playerX = 'X'
-let currPlayer = playerO
-let gameOver = false
-const message = document.getElementById('message')
+const cells = Array.from(document.querySelectorAll('.grid-item'))
+const message = document.getElementById('status')
 
-window.onload = function () { SetGame() }
+const xPlayer = 'X'
+const oPlayer = 'O'
+let clickCounter = 0
+let xGameMoves = []
+let oGameMoves = []
 
-function SetGame () {
-  board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-  ]
-  const boardDiv = document.createElement('div')
-  boardDiv.setAttribute('id', 'board')
-  for (let r = 0; r < 3; r++) {
-    for (let c = 0; c < 3; c++) {
-      const tile = document.createElement('div')
-      tile.id = r.toString() + '-' + c.toString()
-      tile.classList.add('tile')
+const win1 = [0, 1, 2]
+const win2 = [3, 4, 5]
+const win3 = [6, 7, 8]
+const win4 = [0, 4, 8]
+const win5 = [2, 4, 6]
+const win6 = [0, 3, 6]
+const win7 = [1, 4, 7]
+const win8 = [2, 5, 8]
 
-      if (r === 0 || r === 1) {
-        tile.classList.add('horizontal-line')
-      }
-      if (c === 0 || c === 1) {
-        tile.classList.add('vertical-line')
-      }
-      tile.addEventListener('click', setTile)
-      boardDiv.append(tile)
+const playGame = cells.forEach((cell, index) => {
+  cell.addEventListener('click', () => {
+    if (message.innerHTML === 'Player X turn' || message.innerHTML === 'Player O turn') {
+      handleClick(index)
+      checkWin()
+      recordWin()
     }
-  }
-  document.querySelector('body').replaceChild(boardDiv, document.getElementById('board'))
-}
+  })
+})
 
-function setTile () {
-  if (gameOver) {
-    return
-  }
-  const coords = this.id.split('-')
-  const r = parseInt(coords[0])
-  const c = parseInt(coords[1])
-
-  if (board[r][c] !== ' ') {
-    return
-  }
-  board[r][c] = currPlayer
-  this.innerText = currPlayer
-
-  if (currPlayer === playerO) {
-    currPlayer = playerX
+function handleClick (index) {
+  if (cells[index].innerHTML === '') {
+    clickCounter++
+    togglePlayer()
+    insertSign(index)
   } else {
-    currPlayer = playerO
-  }
-  checkWinner()
-}
-function checkWinner () {
-  // horizontally
-  for (let r = 0; r < 3; r++) {
-    if (board[r][0] === board[r][1] && board[r][1] === board[r][2] && board[r][0] !== ' ') {
-      for (let i = 0; i < 3; i++) {
-        const tile = document.getElementById(r.toString() + '-' + i.toString())
-        tile.classList.add('winner')
-      }
-      gameOver = true
-      return
-    }
-  }
-  // vertically
-  for (let c = 0; c < 3; c++) {
-    if (board[0][c] === board[1][c] && board[1][c] === board[2][c] && board[0][c] !== ' ') {
-      for (let i = 0; i < 3; i++) {
-        const tile = document.getElementById(i.toString() + '-' + c.toString())
-        tile.classList.add('winner')
-      }
-      gameOver = true
-      return
-    }
-  }
-
-  // diagonally
-  if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== ' ') {
-    for (let i = 0; i < 3; i++) {
-      const tile = document.getElementById(i.toString() + '-' + i.toString())
-      tile.classList.add('winner')
-    }
-    gameOver = true
-    return
-  }
-
-  // diagonally
-  if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== ' ') {
-    let tile = document.getElementById('0-2')
-    tile.classList.add('winner')
-    tile = document.getElementById('1-1')
-    tile.classList.add('winner')
-    tile = document.getElementById('2-0')
-    tile.classList.add('winner')
-    gameOver = true
+    return false
   }
 }
-const btnReset = document.getElementById('reset')
+function insertSign (index) {
+  if (clickCounter % 2 === 0) {
+    cells[index].innerHTML = oPlayer
+    cells[index].classList.add('noHover')
+    oGameMoves.push(index)
+  } else {
+    cells[index].innerHTML = xPlayer
+    cells[index].classList.add('noHover')
+    xGameMoves.push(index)
+  }
+}
+function togglePlayer () {
+  if (clickCounter % 2 === 0) {
+    message.innerHTML = 'Player X turn'
+  } else {
+    message.innerHTML = 'Player O turn'
+  }
+}
+function containsAll (condition, moves) {
+  return condition.every(i => moves.includes(i))
+}
 
-btnReset.addEventListener('click', () => SetGame())
+function tie () {
+  if (clickCounter >= 9) {
+    message.innerHTML = 'This game is a tie'
+    message.classList.add('win-text')
+  }
+}
+
+function getEmptyCells () {
+  cells.forEach((cell) => {
+    if (cell.innerHTML === '') {
+      cell.classList.add('emptyCells', 'noHover')
+    }
+  })
+}
+
+function checkWin () {
+  if (containsAll(win1, xGameMoves) || containsAll(win2, xGameMoves) || containsAll(win3, xGameMoves) ||
+    containsAll(win4, xGameMoves) || containsAll(win5, xGameMoves) || containsAll(win6, xGameMoves) ||
+    containsAll(win7, xGameMoves) || containsAll(win8, xGameMoves)) {
+    message.innerHTML = 'X player won'
+    getEmptyCells()
+    message.classList.add('win-text')
+  } else if (containsAll(win1, oGameMoves) || containsAll(win2, oGameMoves) || containsAll(win3, oGameMoves) ||
+    containsAll(win4, oGameMoves) || containsAll(win5, oGameMoves) || containsAll(win6, oGameMoves) ||
+    containsAll(win7, oGameMoves) || containsAll(win8, oGameMoves)) {
+    message.innerHTML = 'O player won'
+    getEmptyCells()
+    message.classList.add('win-text')
+  } else {
+    tie()
+  }
+}
+const restartBtn = document.querySelector('.restart')
+restartBtn.addEventListener('click', () => restartGame())
+function restartGame () {
+  clickCounter = 0
+  xGameMoves = []
+  oGameMoves = []
+  message.innerHTML = 'Player X turn'
+  message.classList.remove('win-text')
+  cells.forEach((cell) => {
+    cell.innerHTML = ''
+    cell.classList.remove('noHover', 'emptyCells')
+  })
+}
+const xWon = document.getElementById('xWin')
+const oWon = document.getElementById('0Win')
+
+function recordWin () {
+  let xWin = 0
+  let oWin = 0
+  if (message.innerHTML === 'X player won') {
+    xWin++
+    xWon.innerHTML = `X-Wins: ${xWin}`
+  } if (message.innerHTML === 'X player won') {
+    oWin++
+    oWon.innerHTML = `O-Wins: ${oWin}`
+  }
+}
+playGame()
